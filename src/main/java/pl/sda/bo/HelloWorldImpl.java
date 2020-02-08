7580e2d46a16a42b5b9a5de5796708f6a726e231
MSI <wydrasz3@gmail.com>
2020-02-08 15:25:29 +0100

    propertisy

--- src/main/java/pl/sda/bo/HelloWorldImpl.java
+++ src/main/java/pl/sda/bo/HelloWorldImpl.java
@@ -1,12 +1,16 @@
 package pl.sda.bo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HelloWorldImpl implements HelloWorld {
 
+    @Value("${output.dir}")
+    private String fileDirectory;
+
     @Override
     public void printHelloWorld(String msg) {
-        System.out.println("Hello World !!");
+        System.out.println(msg + " " + fileDirectory);
     }
 }
--- src/main/java/pl/sda/config/AppConfiguration.java
+++ src/main/java/pl/sda/config/AppConfiguration.java
@@ -1,11 +1,20 @@
 package pl.sda.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @ComponentScan("pl.sda.bo")
+@PropertySource("application.properties")
 public class AppConfiguration {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 
 //    @Bean
 //    public HelloWorld helloWorld() {
